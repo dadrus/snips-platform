a21b6986895b23bb27dbb86a0ba97b2b0843f7e7
Dimitrij Drus <dadrus@gmx.de>
2019-08-23 19:31:40 +0100

    mounting local timezone and time to have the same time setting in the docker image as in the host Pi

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ docker run --name snips \
   -v ${PWD}/log:/var/log \
   -v ${PWD}/asound.conf:/etc/asound.conf \
   --device /dev/snd \
+  -v /etc/timezone:/etc/timezone:ro \
+  -v /etc/localtime:/etc/localtime:ro \
   -p 1883:1883 \
   --privileged \
   --log-driver none \
